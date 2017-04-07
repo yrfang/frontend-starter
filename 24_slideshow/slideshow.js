ecab33c60bfd1eded83138a675d35bb625d436d4
YR Fang <fyr1116@gmail.com>
2017-04-07 23:20:44 +0800

    index created by jquery

--- 24_slideshow/slideshow.js
+++ 24_slideshow/slideshow.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var slideIndex = 1;
+  var slideIndex = 0;
 
   function plusSlides(n) {
 
@@ -19,11 +19,30 @@ $(document).ready(function() {
   var slides = $('.slides');
   var dots = $('.dots');
 
-  console.log(slides);
+  console.log(slides.length);
 
   for (var i=0; i<slides.length; i++) {
-    var slide = $(slides[i]);
+    var slide = slides[i];
     console.log(slide);
+    console.log(i);
+    $(slide).css("display", "none");
   }
+
+  for (var i=0; i<dots.length; i++) {
+    var dot = dots[i];
+    console.log(dot);
+    console.log(i);
+    $(dot).removeClass("active");
+  }
+
+  if (i = (slides.length-1)) {
+    slideIndex = 0;
+  }
+  if (i < 0) {
+    slideIndex = (slides.length-1);
+  }
+
+  $(slide).css("display", "block");
+  $(dot).addClass("active");
 
 });
