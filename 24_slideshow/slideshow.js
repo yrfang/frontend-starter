14aad2748f93f8dd7e4e95d65bf8d8c42cb3c17b
YR Fang <fyr1116@gmail.com>
2017-04-07 22:07:15 +0800

    No.24 js

---
+++ 24_slideshow/slideshow.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  var slideIndex = 1;
+
+  function plusSlides(n) {
+
+  }
+
+  function currentSlide(n) {
+
+  }
+
+  function showSlides(n) {
+
+
+  }
+
+  var i;
+  var slides = $('.slides');
+  var dots = $('.dots');
+
+  console.log(slides);
+
+  for (var i=0; i<slides.length; i++) {
+    var slide = $(slides[i]);
+    console.log(slide);
+  }
+
+});
