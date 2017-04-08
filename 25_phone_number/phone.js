c3e931c9affe9e0c9b797afce05975e76f5e8f8f
YR Fang <fyr1116@gmail.com>
2017-04-08 21:21:29 +0800

    No.25 added

---
+++ 25_phone_number/phone.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var numbers = "";
+  $(function () {
+    $("#items").selectable({
+      selected: function (event, ui) {
+        $(".ui-selected", this).each(function () {
+            var index = $("#items li").index(this);
+
+            if (index === 9) {
+              numbers += "*";
+            } else if (index === 10) {
+              numbers += 0;
+            } else if (index === 11) {
+              numbers += "#";
+            } else {
+              numbers += (index + 1);
+            }
+
+            $("#info").html(numbers);
+
+            $('#call').on('click', function() {
+              var text = "Please confirm the phone number."
+              $('#overlay').css('display', 'block');
+              $('#text').html(text);
+              $('#confirm').html(numbers);
+            });
+
+            $('#overlay').on('click', function() {
+              $('#overlay').css('display', 'none');
+            });
+        });
+      }
+    });
+  });
+});
