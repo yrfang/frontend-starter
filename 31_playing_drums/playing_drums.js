c87ebe5039d34433b204c2d0fdbd77f007d66ee0
YR Fang <fyr1116@gmail.com>
2017-04-19 22:56:48 +0800

    No.31 playing drums

---
+++ 31_playing_drums/playing_drums.js
@@ -0,0 +1,22 @@
+//ECMAScript
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.target.classList.remove('playing');
+  }
+
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
