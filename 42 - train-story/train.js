9d2265611a07bfbda4c76bd3b618bd8a3682d739
yrfang <fyr1116@gmail.com>
2017-05-21 20:29:35 +0800

    fixed navbar

--- 42 - train-story/train.js
+++ 42 - train-story/train.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 
   $(window).scroll(function(e) {
     if ($(window).scrollTop()>0) {
-      $('.navbar').removeClass('navbar-top');
+      $('nav').css('position', 'fixed');
+      $('nav').css('background-color', '#e7e7e7');
+      $('.navbar-list a').css('color', '#acacac');
     } else {
-      $('.navbar').addClass('navbar-top');
+      $('nav').css('position', '');
+      $('nav').css('background-color', '');
+      $('.navbar-list a').css('color', '');
     }
   })
 
