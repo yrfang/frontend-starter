b0b25b31f877f776c190acb2a178a58c24e85928
yrfang <fyr1116@gmail.com>
2017-08-09 17:55:30 +0800

    to d20 directory change

--- click_limitation/click_limitation.js
+++ click_limitation/click_limitation.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
         $('#result').text('That\'s ' + speed + ' clicks per minute.');
       }
 
-      $('#try').html('<a href="index.html">Try Again?</a>');
+      $('#try').html('<a href="click_limitation.html">Try Again?</a>');
 
     }, 15000);
 
